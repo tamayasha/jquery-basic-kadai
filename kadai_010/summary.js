6355227197a262fefaa16e46786dede31d981178
tamayasha <158170394+tamayasha@users.noreply.github.com>
2024-04-18 07:04:58 +0900

    課題１０提出用

---
+++ kadai_010/summary.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $('#change-color').on('click',function(){
+        $('#target').css('color','red');
+    });
+
+    $('#change-text').on('click',function(){
+        $('#target').text('こんばんは');
+    });
+
+    $('#fade-out').on('click',function(){
+        $('#target').fadeOut(500);
+    });
+
+    $('#fade-in').on('click',function(){
+        $('#target').fadeIn(500);
+    });
+
+});
